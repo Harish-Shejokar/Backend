2d568b3176fb928c44d3a83bb9c31b8f81a47a06
Harish-Shejokar <90904311+Harish-Shejokar@users.noreply.github.com>
2023-07-20 17:57:15 +0530

    understand export

---
+++ module-1/routes.js
@@ -0,0 +1,82 @@
+const fs = require("fs");   
+
+const requestHandler = (res, req) => {
+  const url = req.url;
+  const method = req.method;
+
+  // console.log(url);
+
+  if (url === "/") {
+    fs.readFile("message.txt", { encoding: "utf-8" }, (err, data) => {
+      if (err) console.log(err);
+
+      res.write("<html>");
+      res.write("<head><title>Enter Message</title></head>");
+      res.write(`<body><h1>${data}</h1></body>`);
+      res.write(
+        `<body><h1>Form page</h1><form action='/message' method='POST'><input type='text' name='message'><button type='submit'>Send</button></form></body>`
+      );
+      res.write("</html>");
+      return res.end();
+    });
+
+    //  res.write("<html>");
+    //  res.write("<head><title>Enter Message</title></head>");
+    //  res.write(
+    //    `<body><h1>Form page</h1><h2>${data}</h2><form action='/message' method='POST'><input type='text' name='message'><button type='submit'>Send</button></form></body>`
+    //  );
+    //  res.write("</html>");
+    //  return res.end();
+  }
+
+  if (url === "/message" && method === "POST") {
+    console.log("message block");
+    const body = [];
+    req.on("data", (chunk) => {
+      console.log(chunk);
+      body.push(chunk);
+    });
+
+    return req.on("end", () => {
+      const parseBody = Buffer.concat(body).toString();
+      console.log(parseBody);
+      const message = parseBody.split("=")[1];
+      fs.writeFile("message.txt", message, (err) => {
+        res.statusCode = 302;
+        res.setHeader("Location", "/");
+        return res.end();
+      });
+    });
+
+    res.write("<html>");
+    res.write("<head><title>Message Page</title></head>");
+
+    // res.write("<body><h1>Message Page</h1></body>");
+    // res.write("</html>");
+  }
+
+  if (url === "/about") {
+    res.write("<html>");
+    res.write("<head><title>Message Page</title></head>");
+
+    res.write("<body><h1>about us  Page</h1></body>");
+    res.write("</html>");
+
+    return res.end();
+  }
+
+  // res.setHeader("Content-Type", "text/html");
+  // res.write("<html>");
+  // res.write("<head><title>My first page</title></head>");
+
+  // res.write(
+  //   "<body><h1>hellow this is my first node js server!!!!!</h1></body>"
+  // );
+  // res.write("</html>");
+  // res.end();
+
+
+  // console.log("hey");
+};
+
+module.exports = requestHandler;
