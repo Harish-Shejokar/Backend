e6423e946da3173c6c341b67fa42c68f1b276473
Harish-Shejokar <90904311+Harish-Shejokar@users.noreply.github.com>
2023-07-24 17:24:37 +0530

    understand routes in express

--- module-1/app.js
+++ module-1/app.js
@@ -1,17 +1,32 @@
-const http = require("http");
+
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
-app.use((req, res, next) => {
-    console.log("first middleware");
-    next();
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.post('/product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
+})
+
+app.use('/add-product',(req, res, next) => {
+    // console.log("first middleware");
+    res.send(`<h1>add product page</h1>
+        <form action='/product' method='post'>
+        <input type='text' name='title'>
+        <input type='number' name='quantity'>
+        <button type='submit'>add-product</button>
+        </form>  
+    `);
+    
 });
 
-app.use((req, res, next) => {
-    console.log("second middleware");
-    // res.send('<h1> epxress frame work </h1>');
-    res.send({"key":"one"})
+app.use('/',(req, res, next) => {
+    // console.log("second middleware");
+    res.send('<h1> HOme Page </h1>');
+    // res.send({"key":"one"})
 });
 
 // const server = http.createServer(app);
